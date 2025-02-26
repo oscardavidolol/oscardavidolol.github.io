a33d9f3a243eafebc03aa357a26ff9b03fbc0039
oscardavidolol <oscardavidolol@gmail.com>
2025-02-26 14:10:43 -0600

    Add files via upload

---
+++ scripttarea1.js
@@ -0,0 +1,16 @@
+let nombre= "hola cristina";
+console.log(nombre+ " ");
+console.log(`${nombre} `);
+let descripcion="gracias por tu compra";
+
+
+let respuesta="Confimamos la compra";
+let cliente="Cristina";
+let numerodeReferencia="6074832";
+let fecha="03/08/2023";
+let evento="Jonas Brothers: FIVE ALBUMS.ONE NIGHT en palau sant jordi";
+let totalEntradas="2";
+let entregasDeEntradas="Entrada Digital Movil";
+let formaDePago="Paypal";
+let TotalPagado="215,00";
+let impuestosygastosdegestionincluidos
